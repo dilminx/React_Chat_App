a87cba1409ef656e49031f1dc6d98484be858b28
dilminx <pdminipura@gmail.com>
2024-08-28 00:01:09 +0530

    change url

--- server/index.js
+++ server/index.js
@@ -9,11 +9,11 @@ app.use(cors());
 const server = http.createServer(app);
 
 const io = new Server(server, {
-  cors: {
-    origin: "https://react-chat-app-frontend-eight.vercel.app",
-    methods: ["GET", "POST"],
-  },
-});
+    cors: {
+      origin: "https://react-chat-app-frontend-eight.vercel.app",
+      methods: ["GET", "POST"],
+    },
+  });
 
 io.on("connection", (socket) => {
     console.log(`Connecting to ${socket.id}`);
